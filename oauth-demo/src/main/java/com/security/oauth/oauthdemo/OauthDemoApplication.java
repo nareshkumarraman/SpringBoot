1da12d98bfb6e82223f8fa6d79775978bd111956
Naresh Kumar Raman <Naresh.Kumar.Raman@hiq.se>
2018-01-23 19:40:33 +0100

    added syso

--- oauth-demo/src/main/java/com/security/oauth/oauthdemo/OauthDemoApplication.java
+++ oauth-demo/src/main/java/com/security/oauth/oauthdemo/OauthDemoApplication.java
@@ -9,5 +9,6 @@ public class OauthDemoApplication extends SpringServletContainerInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(OauthDemoApplication.class, args);
+		System.out.println("Added master");
 	}
 }
